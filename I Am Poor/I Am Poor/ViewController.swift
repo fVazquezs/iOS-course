1e8063e30af8d3f56c924a22cf97991fd11cc00e
Fernando <vzs.fernando@gmail.com>
2020-04-21 18:47:13 -0300

    Initial commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Fernando Vázquez on 21/04/20.
+//  Copyright © 2020 Fernando Vázquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
